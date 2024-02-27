969d65b929a8080e010aff0d304a8a5aba55ec8b
leon-bi <leonbichanga@gmail.com>
2024-02-27 07:51:36 +0300

    scaffold of the app

---
+++ lib/generate_qrcode.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/main.dart';
+
+class GenerateQRCode extends StatefulWidget {
+  const GenerateQRCode({super.key});
+
+  @override
+  State<GenerateQRCode> createState() => _GenerateQRCodeState();
+}
+
+class _GenerateQRCodeState extends State<GenerateQRCode> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/generate_qrcode.dart';
+import 'package:qr_code_scanner/scan_qrcode.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("QR Code Scanner and Generetor"),
+      ),
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const ScanQRCode()));
+                  });
+                },
+                child: const Text('Scan QR code')),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const GenerateQRCode()));
+                  });
+                },
+                child: const Text('Generate QR code'))
+          ],
+        ),
+      ),
+    );
+  }
+}
